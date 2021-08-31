71cb9995b089e42fb7c3995d5072a15b1d5e1ce9
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-08-30 23:31:04 -0500

    fixed loading of js, moved script.js after jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
 // DOM elements
 var currentDayEl = $("#currentDay");
 
-var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-currentDayEl.text(rightNow);
-
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     currentDayEl.text(rightNow);
-    console.log(rightNow);
+    // console.log(rightNow);
 }
 
 setInterval(displayTime, 1000);
