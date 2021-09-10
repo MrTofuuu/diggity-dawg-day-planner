d05de57a1eeff4e93fb997bf6721ed30a6771286
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-09-09 22:10:36 -0500

    changed number formatting in hour blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ var currentDayEl = $("#currentDay");
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var hourNow = moment().format('HH')
     currentDayEl.text(rightNow);
-    // console.log(rightNow);
+    console.log(hourNow);
 }
 
 setInterval(displayTime, 1000);
